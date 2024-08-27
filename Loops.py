c5a763ae6d9da8c7f4756ca9ab3e018f591eede7
Moosa-AI <fakharemoosa@outlook.com>
2024-08-27 11:23:13 +0500

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,19 @@
+# y=1,
+# x=(y, +1)
+# for i in x:
+#     print(i)
+
+
+# name = 'Fakhar e Moosa'
+
+# for i in name:
+#     print(i)
+#     if(i=="k"):
+#         print("Good")
+        
+# name = ['Fakhar e Moosa']
+# for name in name:
+#     print(name)
+
+for k in range(0, 25, 5):
+    print(k)
